34605284bd3e354032ba1ee4362d5938b10ca4b0
Andrea Cazzato <cazzatoandrea04c@gmail.com>
2023-09-27 13:59:33 +0200

    keycloak intragration

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,9 +3,20 @@ import './assets/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 const app = createApp(App)
+import KeyCloakService from "./KeycloakService";
+library.add(faEye, faEyeSlash);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router)
-
+const renderApp = () => {
+    createApp(App).mount("#app");
+};
 app.mount('#app')
+
+KeyCloakService.CallLogin(renderApp);
--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -8,18 +8,20 @@
                     <hr class="border-b border-gray-300 dark:border-gray-600 mb-2" />
                     <!-- Testo centrato sotto la linea nera -->
                     <div class="flex items-center justify-center">
-                        <div class="text-zinc-800 text-3xl font-bold dark:text-zinc-300 text-center" v-show="!isLoginFormVisible">
+                        <div class="text-zinc-800 text-3xl font-bold dark:text-zinc-300 text-center"
+                            v-show="!isLoginFormVisible">
                             Register</div>
-                        <div class="text-zinc-800 text-3xl font-bold dark:text-zinc-300 text-center" v-show="isLoginFormVisible">Welcome
+                        <div class="text-zinc-800 text-3xl font-bold dark:text-zinc-300 text-center"
+                            v-show="isLoginFormVisible">Welcome
                             to Rentella</div>
                     </div>
-                    <p class="flex items-center justify-center text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center"
+                    <p class="flex items-center justify-center text-lg font-normal  text-gray-900 dark:text-white text-center"
                         v-show="!isLoginFormVisible">
                         Create an Account for FREE</p>
-                    <p class="flex items-center justify-center text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center"
+                    <p class="flex items-center justify-center text-lg font-normal  text-gray-900 dark:text-white text-center"
                         v-show="isLoginFormVisible">
                         The easiest way to rent an Umbrella on the beach.</p>
-                    <form class="mx-6 " >
+                    <form class="mx-6 ">
                         <div class="mb-6" v-show="!isLoginFormVisible">
                             <label for="first-name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Firt
@@ -50,6 +52,15 @@
                             <input type="Password" id="password" v-model="password"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="•••••••••" required>
+                            <!-- Aggiungi un'icona dell'occhio per mostrare/nascondere la password -->
+                            <span class="absolute inset-y-0 right-0 flex items-center pr-2">
+                                <font-awesome-icon :icon="showPassword ? 'eye' : 'eye-slash'"
+                                    @click="togglePasswordVisibility" />
+                            </span>
+                            <a v-show="isLoginFormVisible" href="#"
+                                class="text-sm hover:underline flex-1 text-end  text-gray-900 dark:text-white">Hai
+                                dimenticato
+                                la password?</a>
                         </div>
                         <div class="mb-6" v-show="!isLoginFormVisible">
                             <label for="confirm_password"
@@ -59,20 +70,23 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="•••••••••" required>
                         </div>
-                        <button @click="handleRegistration" 
-                            class="w-full h-12 mb-6 px-6 text-indigo-100 transition-colors duration-150 bg-primary rounded-lg focus:shadow-outline hover:bg-hover" v-show="!isLoginFormVisible">
+                        <button @click="handleRegistration"
+                            class="w-full h-12 mb-6 px-6 text-indigo-100 transition-colors duration-150 bg-primary rounded-lg focus:shadow-outline hover:bg-hover cursor-pointer"
+                            v-show="!isLoginFormVisible">
                             Register
                         </button>
                         <button @click="handleLogin"
-                            class="w-full h-12 mb-6 px-6 text-indigo-100 transition-colors duration-150 bg-primary rounded-lg focus:shadow-outline hover:bg-hover" v-show="isLoginFormVisible">
+                            class="w-full h-12 mb-6 px-6 hover:underline text-indigo-100 transition-colors duration-150 bg-primary rounded-lg focus:shadow-outline hover:bg-hover cursor-pointer"
+                            v-show="isLoginFormVisible">
                             Login
                         </button>
                     </form>
-                    <p class="text-center text-gray-500 dark:text-gray-400" v-show="!isLoginFormVisible">
-                    Already have an account? <a class="font-bold" href="#" @click="showLoginForm"><br>Login</a>
+                    <p class="text-center  text-gray-900 dark:text-white" v-show="!isLoginFormVisible">
+                        Already have an account? <a class="font-bold" href="#" @click="showLoginForm"><br>Login</a>
                     </p>
-                    <p class="text-center text-gray-500 dark:text-gray-400" v-show="isLoginFormVisible">
-                        Don’t have an Account? <a class="font-bold" href="#" @click="showLoginForm"><br>Register for free</a>
+                    <p class="text-center  text-gray-900 dark:text-white" v-show="isLoginFormVisible">
+                        Don’t have an Account? <a class="font-bold hover:underline" href="#" @click="showLoginForm"><br>Register for
+                            free</a>
                     </p>
                 </div>
             </div>
@@ -85,7 +99,7 @@
 
 export default {
     components: {
-  },
+    },
     data() {
         return {
             name: "",
@@ -95,6 +109,7 @@ export default {
             password_confirm: "",
             isLoginFormVisible: true,
             modalVisible: false,
+            showPassword: false,
         };
     },
     methods: {
@@ -112,6 +127,9 @@ export default {
             this.email = "";
             this.password = "";
         },
+        togglePasswordVisibility() {
+            this.showPassword = !this.showPassword;
+        }
     },
 };
 </script>
