40ea196f414df6cd1768299a91f3eb3ddd3bab37
Andrea Cazzato <cazzatoandrea04c@gmail.com>
2023-10-06 13:15:16 +0200

    added vue to docker compose

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,28 +1,5 @@
-import './assets/style.css'
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import KeyCloakService from "./KeycloakService";
-import axios from "axios";
 
-
-// Crea l'applicazione Vue
-const app = createApp(App)
-
-// Usa il router
-app.use(router)
-
-// Funzione per il rendering dell'app
-const renderApp = () => {
-    app.mount('#app');
-};
-
-// Chiama il servizio KeyCloak per l'autenticazione
-KeyCloakService.CallLogin(renderApp);
-
-//axios bearer token
-axios.interceptors.request.use(config => {
-    const token = localStorage.getItem("jwt");
-    config.headers["Authorization"] = `Bearer ${KeyCloakService.GetAccesToken()}`;
-    return config;
-});
+createApp(App).mount('#app')
