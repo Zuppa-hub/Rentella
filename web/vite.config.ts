03adb64c2cac38f5d88b555b00309a6e8d273696
Andrea Cazzato <cazzatoandrea04c@gmail.com>
2023-10-11 12:12:44 +0200

    dockerize vue app
    port is not expose

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {    // <-- this object is added
+    host: true,
+    port: 8000
+  },
+  plugins: [vue()]
 })
