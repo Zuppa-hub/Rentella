38cee6b630006c3a80f76c0ef1a8f150a6565ef2
Andrea Cazzato <cazzatoandrea04c@gmail.com>
2023-10-11 12:29:50 +0200

    port config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {    // <-- this object is added
-    host: true,
     port: 8000
   },
   plugins: [vue()]
